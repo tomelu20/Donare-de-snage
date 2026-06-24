a76cffc3b197c2edc32be37bd45aebe0bb165bb0
Mihai <mihai05.toma@gmail.com>
2026-06-25 00:26:43 +0300

    frontend works perfect

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#e63946' }}>🩸 Sistem Programări Donare Sânge</h1>
+      <p>Frontend-ul a fost reinstalat complet și funcționează perfect!</p>
+    </div>
+  );
+}
+
+export default App;
